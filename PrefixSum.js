c67c842f41fb9c410789a9d2fec762dc2de96797
Prajyot Patil <prajyotbpatil2744@gmail.com>
2025-07-06 23:15:00 +0530

    dsa

---
+++ PrefixSum.js
@@ -0,0 +1,21 @@
+let arr = [1, 2, 3, 4, 5]
+let x = arr.length;
+let suffix = new Array(x)
+
+const prefixSum = (arr) => {
+    for (let i = 1; i < arr.length; i++) {
+        arr[i] = arr[i - 1] + arr[i]
+    }
+    suffix[0] = arr[x-1]
+    console.log(suffix[0])
+    for(let i=1;i<arr.length;i++){
+        suffix[i] = arr[x-1] - arr[i-1]
+    }
+    console.log(suffix)
+
+    return console.log(arr )
+
+}
+
+
+prefixSum(arr)
